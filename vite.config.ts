bd055766d93d3b202aaff1936015c0398f1c5c44
dnadiretonoalvo-rgb <dnadiretonoalvo@gmail.com>
2026-09-18 14:28:57 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Site-DNA/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
